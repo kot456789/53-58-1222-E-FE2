afdd226834cba786dd9dd08fa29e823f8f540107
Eugen Grünwald <eugen.13.70.6@gmail.com>
2023-04-14 16:45:23 +0200

    14.04

---
+++ lesson_1(init lesson)/types(boolean, object, func, bignit) .js
@@ -0,0 +1,48 @@
+// ---------------------
+// Boolean (булевый тип)
+
+// true
+// false
+
+// let a = true
+// let b = false // c кавычками будет string (строчное)
+
+// console.log(typeof(a))
+
+// ----------------------
+// Object
+
+// // массив
+// let array = [1,'2',3,4,true]
+
+// // объект
+// let obj = {
+//     name: 'Tigran',
+//     salary: 1000
+// }
+
+// console.log(typeof(obj))
+// console.log(typeof(array))
+
+// --------------------------
+// Function
+
+// Обяъвление функции
+// function handle(){
+//   console.log(1)
+//   console.log(2)
+//   console.log(3)
+// }
+
+// Вызов функции
+// handle()
+
+
+// console.log(typeof(handle))
+
+// -----------------------------
+// BigInt
+
+// let a = 10000000000000000n
+
+// console.log(typeof(a))
