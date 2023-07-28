915ef6828fe29b6db79d83fac0356b94c3da78ef
Eugen Grünwald <eugen.13.70.6@gmail.com>
2023-07-28 11:51:42 +0200

    28.07.2023

--- algoritm_1/alg_1.js
+++ algoritm_1/alg_1.js
@@ -76,25 +76,39 @@
 
 
 
-const array = [1, 2, 3, 5, 6, 8, 9, -1, -5]
-let min3 = Infinity
-let min5 = Infinity
+// const array = [1, 2, 3, 5, 6, 8, 9, -1, -5]
+// let min3 = Infinity
+// let min5 = Infinity
 
-array.forEach((element) => {
-  if (element < min3) {
-    min3 = element
-  }
+// array.forEach((element) => {
+//   if (element < min3) {
+//     min3 = element
+//   }
 
-  if (element < min5 && element > min3) {
-    min5 = element
-  }
-})
+//   if (element < min5 && element > min3) {
+//     min5 = element
+//   }
+// })
 
-console.log(`Минимальное значение, ближайшее к 3: ${min3}`);
-console.log(`Минимальное значение, ближайшее к 5: ${min5}`);
+// console.log(`Минимальное значение, ближайшее к 3: ${min3}`);
+// console.log(`Минимальное значение, ближайшее к 5: ${min5}`);
 
 
 
 // Минимальное значение, ближайшее к 3: 1
 // Минимальное значение, ближайшее к 5: 2
 // Обратите внимание, что алгоритм ищет минимальные значения ближайшие к 3 и 5, но не обязательно равные им. Если необходимо искать значения, равные 3 и 5 по минимальности, то условие во втором if-блоке должно быть изменено на element <= min5 && element >= min3.
+
+
+
+let names = [
+  {id: 1, name: 'Steven', image: 'https://i.ibb.co/tXqmKKR/1.png', salary: '5000'},
+  {id: 2, name: 'Neena', image: 'https://i.ibb.co/yYxK3cq/2.png', salary: '10000'},
+  {id: 3, name: 'John', image: 'https://i.ibb.co/SNqFDbB/3.png', salary: '4500'},
+  {id: 4, name: 'Rogers', image: 'https://i.ibb.co/WnG0fVZ/4.png', salary: '3400'},
+  {id: 5, name: 'Cevin', image: 'https://i.ibb.co/vdQK54J/6.png', salary: '500'},
+  {id: 6, name: 'Alshey', image: 'https://i.ibb.co/wSJN6db/5.png', salary: '1210'},
+  {id: 7, name: 'Milki', image: 'https://i.ibb.co/JpnDnH5/7.png', salary: '1210'},
+  {id: 8, name: 'Polar', image: 'https://i.ibb.co/SnNLhgg/9.png', salary: '1210'},
+  {id: 9, name: 'Grindell', image: 'https://i.ibb.co/fGH3RPW/8.png', salary: '1210'},
+]
