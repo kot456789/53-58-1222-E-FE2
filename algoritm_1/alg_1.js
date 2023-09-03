ee39b6ee2be7b019d74bae5b2eff8a3a4b466aa3
Eugen Grünwald <eugen.13.70.6@gmail.com>
2023-09-03 17:17:53 +0200

    25.08.23

--- algoritm_1/alg_1.js
+++ algoritm_1/alg_1.js
@@ -101,14 +101,52 @@
 
 
 
-let names = [
-  {id: 1, name: 'Steven', image: 'https://i.ibb.co/tXqmKKR/1.png', salary: '5000'},
-  {id: 2, name: 'Neena', image: 'https://i.ibb.co/yYxK3cq/2.png', salary: '10000'},
-  {id: 3, name: 'John', image: 'https://i.ibb.co/SNqFDbB/3.png', salary: '4500'},
-  {id: 4, name: 'Rogers', image: 'https://i.ibb.co/WnG0fVZ/4.png', salary: '3400'},
-  {id: 5, name: 'Cevin', image: 'https://i.ibb.co/vdQK54J/6.png', salary: '500'},
-  {id: 6, name: 'Alshey', image: 'https://i.ibb.co/wSJN6db/5.png', salary: '1210'},
-  {id: 7, name: 'Milki', image: 'https://i.ibb.co/JpnDnH5/7.png', salary: '1210'},
-  {id: 8, name: 'Polar', image: 'https://i.ibb.co/SnNLhgg/9.png', salary: '1210'},
-  {id: 9, name: 'Grindell', image: 'https://i.ibb.co/fGH3RPW/8.png', salary: '1210'},
-]
+// let names = [
+//   {id: 1, name: 'Steven', image: 'https://i.ibb.co/tXqmKKR/1.png', salary: '5000'},
+//   {id: 2, name: 'Neena', image: 'https://i.ibb.co/yYxK3cq/2.png', salary: '10000'},
+//   {id: 3, name: 'John', image: 'https://i.ibb.co/SNqFDbB/3.png', salary: '4500'},
+//   {id: 4, name: 'Rogers', image: 'https://i.ibb.co/WnG0fVZ/4.png', salary: '3400'},
+//   {id: 5, name: 'Cevin', image: 'https://i.ibb.co/vdQK54J/6.png', salary: '500'},
+//   {id: 6, name: 'Alshey', image: 'https://i.ibb.co/wSJN6db/5.png', salary: '1210'},
+//   {id: 7, name: 'Milki', image: 'https://i.ibb.co/JpnDnH5/7.png', salary: '1210'},
+//   {id: 8, name: 'Polar', image: 'https://i.ibb.co/SnNLhgg/9.png', salary: '1210'},
+//   {id: 9, name: 'Grindell', image: 'https://i.ibb.co/fGH3RPW/8.png', salary: '1210'},
+// ]
+
+
+// ---------------------------------------------
+
+const array = [1, 2, 3, 5, 6, 8, 9, -1, -5];
+
+// Инициализация переменных для хранения чисел 3 и 5 по минимальности
+let min3 = Infinity;
+let min5 = Infinity;
+
+// Итерация по элементам массива
+for (let i = 0; i < array.length; i++) {
+  const num = array[i];
+
+  // Если текущий элемент меньше числа 3 по минимальности и не является отрицательным числом
+  if (num < min3 && num !== -Infinity) {
+    min3 = num;
+  }
+
+  // Если текущий элемент меньше числа 5 по минимальности и не является отрицательным числом
+  if (num < min5 && num !== -Infinity) {
+    // Если текущий элемент меньше числа 3 по минимальности, обновляем значения и для числа 3
+    if (num < min3) {
+      min5 = min3;
+      min3 = num;
+    } else {
+      min5 = num;
+    }
+  }
+}
+
+// Проверка найденных значений и вывод результата
+if (min3 !== Infinity && min5 !== Infinity) {
+  console.log("Минимальное число 3: " + min3);
+  console.log("Минимальное число 5: " + min5);
+} else {
+  console.log("В массиве нет нужных чисел");
+}
